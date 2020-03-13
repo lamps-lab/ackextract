98a591828170be2aa3e426b6c7057c50297e4a02
Pei Wang <pwang001@odu.edu>
2020-03-13 15:25:13 -0400

    3.13.2020

---
+++ testPDF.py
@@ -0,0 +1,7 @@
+from ackseer import *
+
+
+print(grobid('Bell_JournMarket_2011_q5J2.pdf'))
+print(pdfAckNer('Bell_JournMarket_2011_q5J2.pdf'))
+
+
---
+++ testXML.py
@@ -0,0 +1,6 @@
+from ackseer import *
+
+print(xmlAckNer('Bell_JournMarket_2011_q5J2.pdf.tei'))
+
+
+
